e4cf28f23c117e8cb4e6f230f7f4a8b3da16ba3b
surajrajegore <rajegoresuraj2880@gmail.com>
2022-08-29 14:30:40 +0530

    Added:check default envirnment using logging

--- src/main/java/com/example/addressbookproject/AddressBookProjectApplication.java
+++ src/main/java/com/example/addressbookproject/AddressBookProjectApplication.java
@@ -1,14 +1,19 @@
 package com.example.addressbookproject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookProjectApplication.class, args);
-        System.out.println("Welcome to Address Book");
+        ApplicationContext context = SpringApplication
+                .run(AddressBookProjectApplication.class, args);
+        log.info("Address Book App Started In {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("AddressBook    DB User name is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
